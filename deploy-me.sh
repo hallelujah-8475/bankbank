c4e2dd2dabc16d20f557b3c826ee731ecaea5e57
Owner <Owner@192.168.10.8>
2021-09-17 23:43:41 +0900

    修正

--- deploy-me.sh
+++ deploy-me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd /home/ec2-user/bankbank/ && git pull
-mv /home/ec2-user/bankbank/target/bankbank.jar /home/ec2-user/
+cd /home/ec2-user/bankbank/ && git pull && mv /home/ec2-user/bankbank/target/bankbank.jar /home/ec2-user/
