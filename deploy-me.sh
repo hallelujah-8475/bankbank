0b69536b7691c44583a27dee97bce02c7b86cca8
Owner <Owner@localhost>
2021-10-16 15:39:29 +0900

    circleci色々修正

--- deploy-me.sh
+++ deploy-me.sh
@@ -2,7 +2,4 @@
 
 cd /home/ec2-user/bankbank/ &&¥
 git pull &&¥
-cp /home/ec2-user/application.properties /home/ec2-user/bankbank/src/main/resources &&¥
-cd /home/ec2-user/bankbank &&¥
-mvn clean dependency:copy-dependencies -DoutputDirectory=lib antrun:run package &&¥
-cp /home/ec2-user/bankbank/target/bankbank.jar /home/ec2-user
+cp /home/ec2-user/application.properties /home/ec2-user/bankbank/src/main/resources
---
+++ deploy-me2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp /home/ec2-user/bankbank/target/bankbank.jar /home/ec2-user
