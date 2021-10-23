d29ee3c507ad5f037a3294552a98cc78fee2fa9a
Owner <Owner@localhost>
2021-10-23 14:03:01 +0900

    環境調整

--- deploy-me.sh
+++ deploy-me.sh
@@ -2,4 +2,5 @@
 
 cd /home/ec2-user/bankbank/ &&¥
 git pull &&¥
-cp /home/ec2-user/application.properties /home/ec2-user/bankbank/src/main/resources
+cp /home/ec2-user/application.properties /home/ec2-user/bankbank/src/main/resources &&¥
+cp /home/ec2-user/bankbank/target/bankbank.jar /home/ec2-user
