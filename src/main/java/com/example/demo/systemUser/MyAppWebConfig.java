ec08a6c232e312b0f3333634c403cf1bc1de9982
Owner <Owner@localhost>
2021-10-23 17:15:12 +0900

    ページング修正

--- src/main/java/com/example/demo/systemUser/MyAppWebConfig.java
+++ src/main/java/com/example/demo/systemUser/MyAppWebConfig.java
@@ -1,19 +1,14 @@
 package com.example.demo.systemUser;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MyAppWebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
-        resolver.setOneIndexedParameters(true);
-        argumentResolvers.add(resolver);
-    }
+//    @Override
+//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+//        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+//        argumentResolvers.add(resolver);
+//    }
 }
